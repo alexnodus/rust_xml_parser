8f9f2cae24f8f2030fa8fcce29120d36d32d2dcf
Aleks Nodus <alexnodus@gmail.com>
2024-02-24 22:16:46 +0300

    The first simple version reads the data section in xml Первая простая версия читающая секцию data  в xml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod parser;
+
+use config::args::Config;
+use std::fs;
+use std::collections::HashSet;
+
+// - Чтобы исправить проблему с "крякозябрами" в PowerShell или командной строке Windows, переключитесь на использование кодовой страницы UTF-8 выполнив следующую команду в терминале:
+//  chcp 65001
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::new();
+    println!("Processing file: {}", config.filename);
+    println!("Обрабатывается файл: {}", config.filename);
+
+
+    println!("{}", &config.filename);
+    let tags = parser::xml_parser::parse_xml(&config.filename)?;
+
+    // Генерация HTML-документа с уникальными элементами
+    // Generating an HTML document with unique elements
+    let mut html_content = String::from("<html><body><ul>");
+    for element in tags {
+        html_content.push_str(&format!("<li>{}</li>", element));
+    }
+    html_content.push_str("</ul></body></html>");
+
+    fs::write("unique_elements.html", html_content)?;
+    println!("HTML файл с уникальными элементами был создан.");
+    println!("An HTML file with unique elements has been created.");
+    
+    Ok(())
 }
--- src/parser/xml_parser.rs
+++ src/parser/xml_parser.rs
@@ -14,14 +14,17 @@ pub fn parse_xml(filename: &str) -> Result<HashSet<String>, Box<dyn std::error::
     for e in parser {
         match e? {
             XmlEvent::StartElement { name, .. } => {
-                if name.local_name == "v8de:Data" {
+                let prefix = name.prefix.unwrap_or("No namespace".to_string());
+                println!("Start element: {}, prefix: {}", name.local_name, prefix);
+                if name.local_name == "Data" && prefix == "v8de" {
                     inside_data = true;
                 } else if inside_data {
                     tags.insert(name.local_name);
                 }
             }
             XmlEvent::EndElement { name } => {
-                if name.local_name == "v8de:Data" {
+                println!("End element: {}", name.local_name);
+                if name.local_name == "Data" {
                     inside_data = false;
                 }
             }
